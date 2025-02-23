32d7c3089f81de4640f389304654e011756b99db
wrdtlkhoir <19623298@std.stei.itb.ac.id>
2025-02-24 02:18:01 +0700

    fix: debugging blok/pieces

--- src/Blok.java
+++ src/Blok.java
@@ -1,69 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Blok {
-    private int id; // maksimal 26 blok
-    private char[][] shape;
+    private List<char[][]> shape;
 
-    public Blok(int id, char[][] shape) {
-        this.id = id;
-        this.shape = shape;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public char[][] getShape() {
+    public List<char[][]> getShape() {
         return shape;
     }
-
+    
+    public Blok(char[][] shapeOri) {
+        shape = new ArrayList<>();
+        perubahanBentuk(shapeOri); 
+    }
+    // Perubahan bentuk blok
+    private void perubahanBentuk(char[][] shapeOri) {
+        char[][] temp = shapeOri;
+        for (int i=0;i<4;i++){
+            shape.add(temp);
+            shape.add(pencerminan(temp));
+            temp = rotasi(temp);
+        }
+    }
     /* Fungsi rotasi blok */
     public char[][] rotasi(char[][] shape) { // rotasi per 90 derajat searah jarum jam
         int baris = shape.length;
         int kolom = shape[0].length;
-        char[][] result = new char[kolom][baris];
+        char[][] hasil_rotasi = new char[kolom][baris];
         for (int i = 0; i < baris; i++) {
             for (int j = 0; j < kolom; j++) {
-                result[j][baris - 1 - i] = shape[i][j];
+                hasil_rotasi[j][baris - 1 - i] = shape[i][j];
             }
         }
-        return result;
+        return hasil_rotasi;
     }
 
     /* Fungsi pencerminan blok */
-    public char[][] pencerminanSumbuY(char[][] shape) { // pencerminan terhadap sumbu vertikal/Y
+    public char[][] pencerminan(char[][] shape) { 
         int baris = shape.length;
         int kolom = shape[0].length;
-        char[][] result = new char[baris][kolom];
+        char[][] hasil_refleksi = new char[baris][kolom];
         for (int i = 0; i < baris; i++) {
             for (int j = 0; j < kolom; j++) {
-                result[i][kolom - 1 - j] = shape[i][j];
+                hasil_refleksi[i][kolom-1-j] = shape[i][j];
             }
         }
-        return result;
-    }
-
-    public char[][] pencerminanSumbuX(char[][] shape) { // pencerminan terhadap sumbu horizontal/X
-        int baris = shape.length;
-        char[][] result = new char[baris][];
-        for (int i = 0; i < baris; i++) {
-            result[i] = shape[baris - 1 - i];
-        }
-        return result;
-    }
-
-    // Perubahan bentuk blok
-    public char[][][] perubahanBentuk() {
-        char[][][] bentuk = new char[11][][];
-        bentuk[0] = shape;
-        bentuk[1] = rotasi(shape);
-        bentuk[2] = rotasi(bentuk[1]);
-        bentuk[3] = rotasi(bentuk[2]);
-        bentuk[4] = pencerminanSumbuY(shape);
-        bentuk[5] = rotasi(bentuk[4]);
-        bentuk[6] = rotasi(bentuk[5]);
-        bentuk[7] = rotasi(bentuk[6]);
-        bentuk[8] = pencerminanSumbuX(shape);
-        bentuk[9] = rotasi(bentuk[8]);
-        bentuk[10] = rotasi(bentuk[9]);
-        return bentuk;
+        return hasil_refleksi;
     }
 }
